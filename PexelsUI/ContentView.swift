f0f6860d9e7e698e8e461cdaa3a9a45f6d066f06
Abdelrahman Mohamed <Obada.semary@gmail.com>
2022-02-03 00:41:09 +0300

    Initial Commit

---
+++ PexelsUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PexelsUI
+//
+//  Created by Abdelrahman Mohamed on 3.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PexelsUI/PexelsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelsUIApp.swift
+//  PexelsUI
+//
+//  Created by Abdelrahman Mohamed on 3.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct PexelsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
